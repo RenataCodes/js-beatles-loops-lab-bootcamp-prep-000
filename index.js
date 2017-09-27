4baf1df25dfd0b202467446f8219e4dad3b9fa4d
RenataCodes <psych.coaching@gmail.com>
2017-09-27 17:55:57 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,16 @@
+function theBeatlesPlay(musicians, instruments) {
+  var array = [];
+for (let i = 0; i < musicians.length; i++) {
+array.push(`${musicians[i]} plays ${instruments[i]}`);
+} return array
+};
+
+
+function johnLennonFacts(facts) {
+var array = [];
+var i = 0;
+  while (facts.length > i) {
+  array.push(facts[i] + "!!!");
+  i++;
+} return array
+  }
